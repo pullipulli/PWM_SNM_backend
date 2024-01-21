513fc4c666be2092ef70e96a4d8968bc703cb523
andrea.pullia <andrea.pullia@studenti.unimi.it>
2024-01-21 17:24:40 +0100

    Initial setup of swaggerUi

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ require('dotenv').config();
 const spotifyInteraction = require('./src/helpers/spotifyAPIInteraction');
 const dbURI = process.env.DB_URI;
 const logRoute = require('./src/middlewares/logRoute');
+
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+
 const app = express();
 const router = express.Router()
 
-//TODO implementare swagger
+//TODO aggiornare lo swagger
 
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
@@ -30,6 +39,7 @@ router.use('/songs', songsRoutes);
 router.use('/playlist', playlistRoutes);
 
 app.use('/api', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3100, "0.0.0.0", async () => {
     log("Server partito sulla porta 3100.");
