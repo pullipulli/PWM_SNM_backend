822a0686dcbb79f0918bed381499ff46ff6817c8
andrea.pullia <andrea.pullia@studenti.unimi.it>
2024-01-23 16:22:12 +0100

    GET all public playlists

---
+++ src/routes/publicPlaylistsRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const {MongoClient: mongoClient} = require("mongodb");
+const dbURI = process.env.DB_URI;
+const router = express.Router()
+
+router.get("/", async (req, res) => {
+    let dbClient = await new mongoClient(dbURI).connect();
+
+    let playlists = await dbClient.db("SNM").collection("playlists").find({
+            privacy: "public"
+        }
+    ).toArray();
+
+    await dbClient.close();
+
+    res.json(playlists);
+});
+
+module.exports = router
